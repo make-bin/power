3cd23dbded9f27e76cdb72e5b8b09b7fda4d93bc
makebin <245048967@qq.com>
2019-08-14 13:55:38 +0800

    20

---
+++ leetcode/20.valid-parentheses.go
@@ -0,0 +1,131 @@
+/*
+ * @lc app=leetcode id=20 lang=golang
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (36.93%)
+ * Total Accepted:    659.7K
+ * Total Submissions: 1.8M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ *
+ *
+ * Note that an empty string is also considered valid.
+ *
+ * Example 1:
+ *
+ *
+ * Input: "()"
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: "()[]{}"
+ * Output: true
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: "(]"
+ * Output: false
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: "([)]"
+ * Output: false
+ *
+ *
+ * Example 5:
+ *
+ *
+ * Input: "{[]}"
+ * Output: true
+ *
+ *
+ */
+
+
+func isValid(s string) bool {
+	stack := New()
+	m := map[string]string{")": "(", "]": "[", "}": "{"}
+
+	for _, v := range s {
+		if _, ok := m[string(v)]; !ok {
+			stack.Push(string(v))
+		} else {
+			if stack.Peek() == m[string(v)] {
+				stack.Pop()
+			} else {
+				return false
+			}
+		}
+	}
+	if stack.Len() != 0 {
+		return false
+	}
+	return true
+}
+
+type (
+	Stack struct {
+		top    *node
+		length int
+	}
+	node struct {
+		value interface{}
+		prev  *node
+	}
+)
+
+// Create a new stack
+func New() *Stack {
+	return &Stack{nil, 0}
+}
+
+// Return the number of items in the stack
+func (this *Stack) Len() int {
+	return this.length
+}
+
+// View the top item on the stack
+func (this *Stack) Peek() interface{} {
+	if this.length == 0 {
+		return nil
+	}
+	return this.top.value
+}
+
+// Pop the top item of the stack and return it
+func (this *Stack) Pop() interface{} {
+	if this.length == 0 {
+		return nil
+	}
+
+	n := this.top
+	this.top = n.prev
+	this.length--
+	return n.value
+}
+
+// Push a value onto the top of the stack
+func (this *Stack) Push(value interface{}) {
+	n := &node{value, this.top}
+	this.top = n
+	this.length++
+}
